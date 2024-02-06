044f0a7057d541c5dcd43d5c975e65dabbcb6319
Josedelpinocas <josedelpino11@gmail.com>
2024-02-06 15:23:35 +0100

    todos los modelos creados

--- app/Models/LogActividad.php
+++ app/Models/LogActividad.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class LogActividad extends Model
 {
     use HasFactory;
+
+    protected $table='logActividades';
+
+
+    protected $fillable = [
+        'usuarioID',
+        'fechaRegistro',
+        'ActividadRealizada',
+    ];
 }
