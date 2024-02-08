58c634b9979747a06040c8c3264ec7112febed2d
Josedelpinocas <josedelpino11@gmail.com>
2024-02-08 18:33:47 +0100

    division de zonas

--- app/Http/Controllers/DispositivoController.php
+++ app/Http/Controllers/DispositivoController.php
@@ -7,56 +7,37 @@ use App\Models\Dispositivo;
 
 class DispositivoController extends Controller
 {
+      /* zona fran */
     public function list()
     {
         $dispositivos = dispositivo::all();
         return view('dispositivos.listaDispositivos', compact('dispositivos'));
     }
+    /*fin zona fran  */
+
+
+    
+    /* zona silvia*/
     public function listarAveriados()
     {
         $dispositivosAveriados = dispositivo::all();
         return view('dispositivos.listaDispositivos', compact('dispositivos'));
     }
-    public function b()
-    {
-        $dispositivos = dispositivo::all();
-        return view('dispositivos.listaDispositivos', compact('dispositivos'));
-    }
-    public function c()
-    {
-        $dispositivos = dispositivo::all();
-        return view('dispositivos.listaDispositivos', compact('dispositivos'));
-    }
-    public function d()
-    {
-        $dispositivos = dispositivo::all();
-        return view('dispositivos.listaDispositivos', compact('dispositivos'));
-    }
-    public function e()
-    {
-        $dispositivos = dispositivo::all();
-        return view('dispositivos.listaDispositivos', compact('dispositivos'));
-    }
-    public function f()
-    {
-        $dispositivos = dispositivo::all();
-        return view('dispositivos.listaDispositivos', compact('dispositivos'));
-    }
-    public function g()
-    {
-        $dispositivos = dispositivo::all();
-        return view('dispositivos.listaDispositivos', compact('dispositivos'));
-    }
-    public function h()
-    {
-        $dispositivos = dispositivo::all();
-        return view('dispositivos.listaDispositivos', compact('dispositivos'));
-    }
-    public function i()
-    {
-        $dispositivos = dispositivo::all();
-        return view('dispositivos.listaDispositivos', compact('dispositivos'));
-    }
+      /*fin zona silvia  */
+
+
+    /* zona jose*/
+
+    /*fin zona jose  */
+
+    
+    /* zona juanma*/
+
+    /*fin zona jose  */
+
+
+
+
 }
 
 
