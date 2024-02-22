980b7c2b5cb6eddacd314961897bea8ef0b1b69e
Josedelpinocas <josedelpino11@gmail.com>
2024-02-22 18:58:13 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 Route::delete('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
 
